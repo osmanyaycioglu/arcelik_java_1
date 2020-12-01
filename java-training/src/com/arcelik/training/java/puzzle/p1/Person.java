ce06fd4f1bb1cd7c52f8b1ef0e23d751cecd67bd
Osman <osman.yaycioglu@gmail.com>
2020-12-01 15:41:47 +0300

    day 3.4

---
+++ java-training/src/com/arcelik/training/java/puzzle/p1/Person.java
@@ -0,0 +1,35 @@
+package com.arcelik.training.java.puzzle.p1;
+
+
+public class Person {
+
+    private String isim;
+    private String soyisim;
+    private String cinsiyet;
+
+    public String getIsim() {
+        return this.isim;
+    }
+
+    public void setIsim(final String isimParam) {
+        this.isim = isimParam;
+    }
+
+    public String getSoyisim() {
+        return this.soyisim;
+    }
+
+    public void setSoyisim(final String soyisimParam) {
+        this.soyisim = soyisimParam;
+    }
+
+    public String getCinsiyet() {
+        return this.cinsiyet;
+    }
+
+    public void setCinsiyet(final String cinsiyetParam) {
+        this.cinsiyet = cinsiyetParam;
+    }
+
+
+}
